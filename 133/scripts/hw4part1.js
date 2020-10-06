d3c05710dddeae24420421c6cd7d6e6f57fd31c0
Yuridia Larios Aispuro <yuriydani@gmail.com>
2020-10-06 09:43:58 -0700

    CNIT-133: Add part 1b of homework4

--- 133/scripts/hw4part1.js
+++ 133/scripts/hw4part1.js
@@ -1,4 +1,4 @@
-//the product and sum of every fourth integer from 5 to 21 inclusive 
+//Part 1a: the product and sum of every fourth integer from 5 to 21 inclusive using for loop
 let containerPart1a = document.getElementById("hwPart1a");
 let productOfEveryFourthInteger = 1;
 let sumOfEveryFourthInteger = 0;
@@ -19,4 +19,27 @@ if (containerPart1a != null) {
 
   containerPart1a.append(productTextParagraph);
   containerPart1a.append(sumTextParagraph);
+}
+
+//Part 1b: the product and sum of every third integer from 3 to 21 inclusive using for while
+let containerPart1b = document.getElementById("hwPart1b");
+let productOfEveryThirdInteger = 1;
+let sumOfEveryThirdInteger = 0;
+let countNumber = 3;
+while (countNumber <= 21) {
+  productOfEveryThirdInteger *= countNumber;
+  sumOfEveryThirdInteger += countNumber;
+  countNumber += 3;
+}
+if (containerPart1b != null) {
+  let productTextParagraph2 = document.createElement("p");
+  productTextParagraph2.classList.add("productResult2");
+  productTextParagraph2.textContent = `The product of every third integer from 3 to 21 inclusive is ${productOfEveryThirdInteger}.`;
+
+  let sumTextParagraph2 = document.createElement("p");
+  sumTextParagraph2.classList.add("sumResult2");
+  sumTextParagraph2.textContent = `The sum of every third integer from 3 to 21 inclusive is ${sumOfEveryThirdInteger}.`;
+
+  containerPart1b.append(productTextParagraph2);
+  containerPart1b.append(sumTextParagraph2);
 }
