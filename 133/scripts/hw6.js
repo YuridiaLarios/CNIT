0909bf8ec9d4e3a158bdcb7a19570017e844b1f8
yuridia <ylaispuro@indeed.com>
2020-12-04 22:57:18 -0800

    CNIT-133: remove bullet points

--- 133/scripts/hw6.js
+++ 133/scripts/hw6.js
@@ -4,20 +4,43 @@ let toNearestTenths;
 let toNearestHundredths;
 let toNearestThousandths;
 
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling);
+},false;
+
+function removeCurrentErrorState() {
+  let errorContainersArray = document.querySelectorAll(".errorContainer");
+  // console.log(errorContainersArray);
+  errorContainersArray.forEach(errorContainer => {errorContainer.style.display = "none"});
+  let errorRedStage = document.getElementsByClassName("errorRedStage");
+  while (errorRedStage.length)
+    errorRedStage[0].className = errorRedStage[0].className.replace(/\berrorRedStage\b/g, "");
+}
+
+
+function createErrorDiv(message, currentContainer) {
+  let errorDiv = document.createElement("div");
+  let errorDivID = currentContainer.getAttribute("id") + "ErrorMessage";
+  errorDiv.setAttribute("id", errorDivID);
+  errorDiv.classList.add("errorContainer");
+  errorDiv.textContent = message;
+  errorDiv.appendAfter(currentContainer);
+}
+
 function printResults(inputFieldValue) {
   let part1Result = document.getElementById("part1Results");
   let result = '';
-  result += `<ul><li>Original Floating point: ${inputFieldValue}</li>`;
-  result += `<li>Rounded to nearest integer: ${toNearestInteger}</li>`;
-  result += `<li>Square root rounded to nearest integer: ${toSquareRootAndInteger}</li>`;
-  result += `<li>Rounded to nearest tenths: ${toNearestTenths}</li>`;
-  result += `<li>Rounded to nearest hundredths ${toNearestHundredths}</li>`;
-  result += `<li>Rounded to nearest Thousandths ${toNearestThousandths}</li></ul>`;
+  result += `<ul><li><span class="liText">Original Floating point:</span> ${inputFieldValue}</li>`;
+  result += `<li><span class="liText">Rounded to nearest integer:</span> ${toNearestInteger}</li>`;
+  result += `<li><span class="liText">Square root rounded to nearest integer:</span> ${toSquareRootAndInteger}</li>`;
+  result += `<li><span class="liText">Rounded to nearest tenths:</span> ${toNearestTenths}</li>`;
+  result += `<li><span class="liText">Rounded to nearest hundredths:</span> ${toNearestHundredths}</li>`;
+  result += `<li><span class="liText">Rounded to nearest Thousandths</span> ${toNearestThousandths}</li></ul>`;
   part1Result.innerHTML = result;
 }
 
 function calculate(inputFieldValue) {
-  /* 
+  /*
   a) round the floating-point number to the nearest integer
   b) calculate the square root of the floating-point number and round it to an integer
   c) round the floating-point number to the nearest tenths position
@@ -26,9 +49,9 @@ function calculate(inputFieldValue) {
   */
   toNearestInteger = Math.round(inputFieldValue);
   toSquareRootAndInteger = Math.round((Math.sqrt(inputFieldValue)));
-  toNearestTenths = Math.round(inputFieldValue * 10) / 10;
-  toNearestHundredths = Math.round(inputFieldValue * 100) / 100;
-  toNearestThousandths = Math.round(inputFieldValue * 1000) / 1000;
+  toNearestTenths = inputFieldValue.toFixed(1);
+  toNearestHundredths = inputFieldValue.toFixed(2);
+  toNearestThousandths = inputFieldValue.toFixed(3);
 
   printResults(inputFieldValue);
 }
@@ -36,11 +59,10 @@ function calculate(inputFieldValue) {
 function makeCalculations() {
   let validInput = false;
   let inputField = document.getElementById("userInputPart1");
-  let inputFieldValue = document.getElementById("userInputPart1").value;
-  if (inputFieldValue !== "" && typeof (parseFloat(inputFieldValue) === "number")) {
-    validInput = true;
-  }
-  if (validInput) {
+  let inputFieldValue = parseFloat(document.getElementById("userInputPart1").value);
+  if (inputFieldValue !== "" && typeof(parseFloat(inputFieldValue) === "number")) {
     calculate(inputFieldValue);
+  } else {
+    createErrorDiv("Error please enter a valid input", inputField);
   }
 }
